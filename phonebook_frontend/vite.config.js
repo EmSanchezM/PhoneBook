f3c954ea6d55c9980c219530940420cef3509f61
Elvin Sanchez <elvin.sanchez-ext@dominion-global.com>
2025-12-06 09:35:11 -0600

    Refactor code structure for improved readability and maintainability

--- phonebook_frontend/vite.config.js
+++ phonebook_frontend/vite.config.js
@@ -3,18 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: '**/*.{jsx,js}',
-    })
-  ],
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
